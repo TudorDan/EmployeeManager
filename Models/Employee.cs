0e1f218083f3ce3a75029763487dcd831ccadd4d
TudorDan <tudor.ist@gmail.com>
2020-09-30 18:53:59 +0300

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Models
+{
+    class Employee
+    {
+        protected int salary;
+        protected string profession;
+
+        public Employee(int salary, string profession)
+        {
+            this.salary = salary;
+            this.profession = profession;
+        }
+
+        public string Profession
+        {
+            get { return profession; }
+            set { profession = value; }
+        }
+
+        public int Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Models
+{
+    class Person : Employee
+    {
+        private string name;
+        private DateTime birthDate;
+        private Gender gender;
+
+        public Person(int salary, string profession, string name, DateTime birthDate, Gender gender) : base(salary, profession)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+            this.gender = gender;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+
+        public Gender Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+    }
+}
